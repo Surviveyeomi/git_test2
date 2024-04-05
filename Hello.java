c71cde90200e0e561dcfd9da781ef4fc0695bd9a
Surviveyeomi <yeomi189@gmail.com>
2024-04-05 17:50:55 +0900

    화면 출력 방법 수정

--- Hello.java
+++ Hello.java
@@ -2,16 +2,10 @@ package git_test2;
 
 public class Hello {
 
-	public void say() {
-		System.out.println("안녕하세요"); 
-	}
-	
-	public Hello(String message) {
-		this.message = message; 
-	}
-	
-	public void say() {
-		System.out.println(message); 
-	}
-	
+    String message = "안녕하세요";
+
+    public String say() {
+			 return message; 
+			}
+    
 }
