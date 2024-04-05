fc08ff71aa167d92f2ac12b1484e93b90353a955
Surviveyeomi <yeomi189@gmail.com>
2024-04-05 16:36:31 +0900

    Hello 클래스 수정

--- Hello.java
+++ Hello.java
@@ -2,10 +2,16 @@ package git_test2;
 
 public class Hello {
 
-    String message = "안녕하세요";
-
-    public void say() {
-        System.out.println("message");
-    }
-    
+	public void say() {
+		System.out.println("안녕하세요"); 
+	}
+	
+	public Hello(String message) {
+		this.message = message; 
+	}
+	
+	public void say() {
+		System.out.println(message); 
+	}
+	
 }
